1f17fc5e94e4fe080c5e9297702c7f6c7e4241bd
Ziron-hub <oaquila14@gmail.com>
2022-04-13 02:41:32 -0300

    Adicionado a lógica de Pesquisa para profissionais

--- AA2/src/main/java/br/ufscar/dc/dsw/Aa2Application.java
+++ AA2/src/main/java/br/ufscar/dc/dsw/Aa2Application.java
@@ -31,17 +31,56 @@ public class Aa2Application {
 
 
 			Professional professional1 = new Professional();
-			professional1.setUsername("professional");
+			professional1.setUsername("professional1");
 			professional1.setName("Aquila Oliveira");
-			professional1.setPassword(encoder.encode("teste"));
+			professional1.setPassword(encoder.encode("oi"));
 			professional1.setRole("PROF");
-			professional1.setCpf("50004053826");
+			professional1.setCpf("50324053826");
 			professional1.setEmail("profissional1@gmail.com");
 			professional1.setExpertise("Terapia Cognitiva");
-			professional1.setKnowledgeArea("Saúde");
+			professional1.setKnowledgeArea("Engenharias");
 			professional1.setQualifications("teste");
 			professional1.setEnabled(true);
 			usuarioDAO.save(professional1);
+
+			Professional professional2 = new Professional();
+			professional2.setUsername("professional2");
+			professional2.setName("Robsvaldo");
+			professional2.setPassword(encoder.encode("oi"));
+			professional2.setRole("PROF");
+			professional2.setCpf("50004053809");
+			professional2.setEmail("profissional2@gmail.com");
+			professional2.setExpertise("Biologia e Espaço Tridimensional");
+			professional2.setKnowledgeArea("Ciências Biológicas");
+			professional2.setQualifications("teste");
+			professional2.setEnabled(true);
+			usuarioDAO.save(professional2);
+			
+			Professional professional3 = new Professional();
+			professional3.setUsername("professional3");
+			professional3.setName("Vini");
+			professional3.setPassword(encoder.encode("oi"));
+			professional3.setRole("PROF");
+			professional3.setCpf("50004053821");
+			professional3.setEmail("profissional3@gmail.com");
+			professional3.setExpertise("Biologia Computacional");
+			professional3.setKnowledgeArea("Ciências Biológicas");
+			professional3.setQualifications("teste");
+			professional3.setEnabled(true);
+			usuarioDAO.save(professional3);
+
+			Professional professional4 = new Professional();
+			professional4.setUsername("professional4");
+			professional4.setName("Santiago");
+			professional4.setPassword(encoder.encode("oi"));
+			professional4.setRole("PROF");
+			professional4.setCpf("50004012312");
+			professional4.setEmail("profissional4@gmail.com");
+			professional4.setExpertise("Lingua Inglesa");
+			professional4.setKnowledgeArea("Linguística, Letras e Artes");
+			professional4.setQualifications("teste");
+			professional4.setEnabled(true);
+			usuarioDAO.save(professional4);
 			
 		};
 	}
